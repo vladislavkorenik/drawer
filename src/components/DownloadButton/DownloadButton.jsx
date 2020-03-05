4fb6d670983178fdcbba9ea0ad034a23451591f5
Vladislav Korenik <vladislavkorenik@gmail.com>
2020-03-05 22:45:56 +0300

    fixed download button

--- src/components/DownloadButton/DownloadButton.jsx
+++ src/components/DownloadButton/DownloadButton.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 export const DownloadButton = ({ href }) => {
   const buttonStyle = href
-    ? "mb-3 btn btn-light stretched-link"
+    ? "mb-3 display-4 btn btn-light stretched-link"
     : "mb-3 alert alert-light";
   const Component = href ? "a" : "p";
   return (
-    <Component href={href} className={buttonStyle}>
+    <Component
+      href={href}
+      download={href ? "output" : null}
+      className={buttonStyle}
+    >
       {href ? "Download canvas" : "Upload your .txt file with commands"}
     </Component>
   );
